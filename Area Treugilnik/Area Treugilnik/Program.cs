15773fd743457fffc151e7857eaf6c07d3e87409
unknown <tastanov.eldar00@mail.ru>
2018-01-25 15:01:57 +0600

    kek

---
+++ Area Treugilnik/Area Treugilnik/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_Treugilnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double q , a = 5, b = 7,c,s;
+            q = (double)Math.Pow(a, 2) + (double)Math.Pow(b, 2);
+            Console.WriteLine(q);
+            c = Math.Sqrt(q);
+            Console.WriteLine(c);
+            s = (a * b) / 2;
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
